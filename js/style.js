9752d174a2d2acc7ff525254c7ee87b1261b9e94
AnneWS <32013118+AnneWS@users.noreply.github.com>
2019-05-21 15:36:59 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -5,34 +5,120 @@ console.log('aarde hype');
 
 var aarde = document.querySelector('img');
 
-aarde.addEventListener('dblclick', function () {
+/*aarde.addEventListener('dblclick', function () {
     if (aarde.src == "../images/earth_PNG39.png") {
         aarde.src = "../images/earth_PNG-bw.png";
     } else {
         aarde.src = "../images/earth_PNG39.png";
     }
-});
+});*/
 
-aarde.onclick = function rotate() {
-    aarde.setAttribute('style', 'transform:rotate(45deg)');
+/*thanks to Joop*/
+function kleurAarde() {
+    aarde.classList.toggle("kleur");
 }
 
-aarde.addEventListener('keydown', function draaien() {
-    var key = event.key;
-    console.log(key);
-    var oldAngle = aarde.style.rotate;
+function grijsAarde() {
+    aarde.classList.remove("kleur");
+}
+
+aarde.addEventListener('click', kleurAarde);
+aarde.addEventListener('dblclick', grijsAarde);
+
+
+/*function keyDownTextField(e) {
+    var keyCode = e.keyCode;
+    var oldAngle = 0;
     console.log(oldAngle);
-    var newAngle = oldAngle + 45;
-    var newAngleBack = newAngle - 45;
-    if (key === 'ArrowRight') {
-        newAngle;
+    var newAngle = oldAngle + 45 + "deg";
+    var newAngleBack = newAngle - 45 + "deg";
+    if (keyCode == 39) {
+        aarde.style.transform = 'rotate(45deg)';
         console.log(newAngle);
     }
-    if (key === 'ArrowLeft') {
-        newAngleBack;
+    if (keyCode == 37) {
+        aarde.style.transform = 'rotate(-45deg)';
         console.log(newAngleBack);
     }
-})
-/*var angle = 0;
-aarde.getAttribute('transform');
-console.log('Rotate: ' + angle + 'deg');*/
+}*/
+
+var goToTop = document.querySelector('h3');
+goToTop.addEventListener('click', function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
+
+/*var oldAngle = 0;
+function keyDownTextField(e) {
+    var keyCode = e.keyCode;
+    console.log(oldAngle);
+    var newAngle = 45;
+    var newAngleBack = 45;
+    if (keyCode == 39) {
+        aarde.style.transform = 'rotate(' + oldAngle + newAngle + "deg)";
+        console.log(newAngle);
+    }
+    if (keyCode == 37) {
+        aarde.style.transform = 'rotate(' + newAngle - newAngleBack + "deg)";
+        console.log(newAngleBack);
+    }
+}
+
+document.addEventListener("keydown", keyDownTextField, false);*/
+
+function draaiAarde(e) {
+    var keyCode = e.keyCode;
+    if (keyCode == 39) {
+        aarde.classList.remove('draaiLinks');
+        aarde.classList.add('draaiRechts');
+    }
+    if (keyCode == 37) {
+        aarde.classList.remove('draaiRechts');
+        aarde.classList.add('draaiLinks');
+    }
+    e.preventDefault;
+}
+
+document.addEventListener('keydown', draaiAarde, false);
+
+
+
+/*var cirkelObj = null;
+
+function init() {
+    var cirkelObj = document.getElementsByClassName('circle');
+    cirkelObj.style.position= 'relative';
+    cirkelObj.style.left = '0px';
+}
+
+function moveRight() {
+    cirkelObj.style.left = parseInt(cirkelObj.style.left) + 10 + 'px';
+}
+
+window.onload = init;
+
+cirkelObj.addEventListener('click', moveRight);*/
+
+/*function verschuiven() {
+    var cirkelX = 0;
+    cirkelX += 5;
+    cirkel.style.position = "relative";
+    cirkel.style.left = cirkelX + "em";
+    verschuiven.preventDefault();
+}
+
+cirkel.addEventListener('click', verschuiven);*/
+var cirkel = document.querySelector('div>svg>circle');
+//console.log(cirkel);
+
+window.addEventListener("scroll", function() {
+    if (window.pageYOffset >= 500){
+        //console.log("check");
+        cirkel.classList.add("moveright");
+    }
+    else {
+        cirkel.classList.remove("moveright");
+    }
+});
